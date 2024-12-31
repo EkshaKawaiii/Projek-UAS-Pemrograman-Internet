69e436e3c3788a10560b3170595e59fc0ea58988
EkshaKawaiii <ekshaoktavian1710@gmail.com>
2024-12-31 16:13:13 +0700

    Delete CV. Rental Mobil Eksha directory

--- CV. Rental Mobil Eksha/hapus_mobil.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'config.php';
-
-$no_plat = $_GET['no_plat'];
-mysqli_query($koneksi, "DELETE FROM tbl_mobil_eksha WHERE no_plat_eksha='$no_plat'");
-header("Location: mobil.php");
-?>
--- CV. Rental Mobil Eksha/hapus_pelanggan.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'config.php';
-
-$nik_ktp = $_GET['nik_ktp'];
-mysqli_query($koneksi, "DELETE FROM tbl_pelanggan_eksha WHERE nik_ktp_eksha='$nik_ktp'");
-header("Location: pelanggan.php");
-?>
